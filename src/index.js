20a882a6f9dc576d39a5d747dea943486b4cf290
jyc-coder <jyc4648@gmail.com>
2022-12-04 17:14:55 +0900

    요구사항 1 솔루션
    `window.addEventListener` 를 사용해서 `load`되는 순간 마다 `focus()`를 호출하게 작성함

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 // TODO: 이 곳에 정답 코드를 작성해주세요.
+// 1. 페이지가 로드 된 시점에 ID 입력 창에 Focus가 되어 있어야 한다.
+// 대상: ID 입력 Input
+// 시점: 페이지가 로드 되었을 때
+// 이벤트 : Focus()
+const $id = document.getElementById('id')
+window.addEventListener('load', () => $id.focus())
