e09eeb4126185261713afb87550f6a0e1877ccf2
jyc-coder <jyc4648@gmail.com>
2022-12-08 16:45:23 +0900

    요구사항2 리팩토링
    메소드 이름을 좀더 직관적으로 변경함
    
    validated** => check**Validation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 // 시점: 페이지가 로드 되었을 때
 // 이벤트 : Focus()
 const $id = document.getElementById('id')
+const $idMsg = document.getElementById('id-msg')
 window.addEventListener('load', () => $id.focus())
 
 // 2. 유효성 검사 로직
@@ -17,35 +18,43 @@ const $pwCheck = document.getElementById('pw-check')
 var ID_REGEX = new RegExp('^[a-z0-9_-]{5,20}$')
 var PW_REGEX = new RegExp('^[a-zA-z0-9]{8,16}$')
 
-const validateId = (value) => {
+const ID_ERROR_MSG = {
+    required: '필수 정보입니다.',
+    invalid: '5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.',
+}
+
+const checkIdValidation = (value) => {
     // (공통) 모든 필드의 값은 빠짐 없이 입력해야 합니다.
     // 5~20자, 영문 소문자 ,숫자 ,특수기호(_),(-)만 사용가능
     const isValidId = ID_REGEX.test(value)
     console.log(isValidId)
 }
+$id.addEventListener('focusout', () => checkIdValidation($id.value))
 
-const validatePw = (value) => {
+const checkPwvalidation = (value) => {
     // 8~16자, 영문 대/소문자, 숫자 사용가능
     const isValidPw = PW_REGEX.test(value)
     console.log(isValidPw)
 }
 
-const validatePwCheck = (value) => {
+$pw.addEventListener('focusout', () => checkPwvalidation($pw.value))
+
+const checkPwCheckValidation = (value) => {
     // 비밀번호와 같은 값이어야함
     const isValidPwCheck = $pw.value === value
     console.log(isValidPwCheck)
 }
 
-$id.addEventListener('focusout', () => validateId($id.value))
-$pw.addEventListener('focusout', () => validatePw($pw.value))
-$pwCheck.addEventListener('focusout', () => validatePwCheck($pwCheck.value))
+$pwCheck.addEventListener('focusout', () =>
+    checkPwCheckValidation($pwCheck.value)
+)
 
 const $submit = document.getElementById('submit')
 
 $submit.addEventListener('click', (e) => {
     e.preventDefault()
     console.log(e)
-    validateId($id.value)
-    validatePw($pw.value)
-    validatePwCheck($pwCheck.value)
+    checkIdValidation($id.value)
+    checkPwvalidation($pw.value)
+    checkPwCheckValidation($pwCheck.value)
 })
