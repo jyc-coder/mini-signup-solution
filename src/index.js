dcfc6bcbe7cf16e2b74d0f1d4cf61e0ae55b180d
jyc-coder <jyc4648@gmail.com>
2022-12-08 16:28:41 +0900

    요구사항 2 솔루션
    input 창의 id 값들을 가져오고`addEventListener`를 사용해서 `focusout`된 경우 유효성 검사메소드를 호출함
    
    - 정규식을 사용해서 사용자가 입력한 데이터가 규칙에 위배되지 않는지 여부를 확인하고 맞으면 true, 아니면 false값으로 결정됨
    
    - console.log 를 사용해서 사용자가 `focusout`상황을 만들었을때 유효성 검사의 결과를 콘솔창에 나타나게 설정함
    
    - `가입하기` 버튼의 경우는 버튼을 클릭한 순간 모든 input 항목의 유효성 검사를 진행하므로 `addEventListener`를 `click`할때 호출되게 설정함

--- src/index.js
+++ src/index.js
@@ -5,3 +5,47 @@
 // 이벤트 : Focus()
 const $id = document.getElementById('id')
 window.addEventListener('load', () => $id.focus())
+
+// 2. 유효성 검사 로직
+// 대상 : ID, 비밀번호, 비밀번호 확인 input
+// 이벤트 : (1) input focus out (2) 가입하기 버튼을 눌렀을 때
+// 핸들러 : (1) 해당 input의 유효성 검사 (2) 모든 필드의 유효성 검사
+
+const $pw = document.getElementById('pw')
+const $pwCheck = document.getElementById('pw-check')
+
+var ID_REGEX = new RegExp('^[a-z0-9_-]{5,20}$')
+var PW_REGEX = new RegExp('^[a-zA-z0-9]{8,16}$')
+
+const validateId = (value) => {
+    // (공통) 모든 필드의 값은 빠짐 없이 입력해야 합니다.
+    // 5~20자, 영문 소문자 ,숫자 ,특수기호(_),(-)만 사용가능
+    const isValidId = ID_REGEX.test(value)
+    console.log(isValidId)
+}
+
+const validatePw = (value) => {
+    // 8~16자, 영문 대/소문자, 숫자 사용가능
+    const isValidPw = PW_REGEX.test(value)
+    console.log(isValidPw)
+}
+
+const validatePwCheck = (value) => {
+    // 비밀번호와 같은 값이어야함
+    const isValidPwCheck = $pw.value === value
+    console.log(isValidPwCheck)
+}
+
+$id.addEventListener('focusout', () => validateId($id.value))
+$pw.addEventListener('focusout', () => validatePw($pw.value))
+$pwCheck.addEventListener('focusout', () => validatePwCheck($pwCheck.value))
+
+const $submit = document.getElementById('submit')
+
+$submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(e)
+    validateId($id.value)
+    validatePw($pw.value)
+    validatePwCheck($pwCheck.value)
+})
